d558feaadbf02efa9217ddb84ea730331cdfd787
HassanMunene <awanzihassan@gmail.com>
2024-02-22 18:33:02 +0300

    setting up headers in the query endpoint instead of the fetchBaseQuery

--- KcaVibes/src/redux/spotifyApi.js
+++ KcaVibes/src/redux/spotifyApi.js
@@ -4,14 +4,16 @@ export const spotifyApi = createApi({
     reducerPath: 'spotifyApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://spotify-web2.p.rapidapi.com',
-        prepareHeader: (headers) => {
-            headers.set('X-RapidAPI-Key', '4068abb194msh49e0eccabdc9126p1b052djsnffd1fa04c572');
-            return headers;
-        },
     }),
     endpoints: (builder) => ({
         getTopHitsInKenya: builder.query({
-            query: (playListId) => `/playlist_tracks/?id=${playListId}`
+            query: (playListId) => ({
+                url: `/playlist_tracks/?id=${playListId}`,
+                method: 'GET',
+                headers: {
+                  'X-RapidAPI-Key': '4068abb194msh49e0eccabdc9126p1b052djsnffd1fa04c572'
+                },
+            }),
         }),
     }),
 });
