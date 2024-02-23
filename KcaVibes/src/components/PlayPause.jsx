b5735fa3ddb2f1658570f5b3cd98007d497c566a
HassanMunene <awanzihassan@gmail.com>
2024-02-23 13:19:37 +0300

    compeleting the playpause functionality

--- KcaVibes/src/components/PlayPause.jsx
+++ KcaVibes/src/components/PlayPause.jsx
@@ -1,5 +1,14 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
+
+const PlayPause = ({song,handlePlay, handlePause, isSongPlaying, activeSong}) => {
+  return (
+    <>
+      {isSongPlaying && activeSong?.name === song.track.name 
+      ? (<FaPauseCircle size={35} className="text-green-500" onClick={handlePause}/>)
+      : (<FaPlayCircle size={35} className="text-green-500" onClick={handlePlay}/>)
+      }
+    </>
+  )
+};
 
 export default PlayPause;
