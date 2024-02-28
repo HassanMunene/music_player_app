062444d597bf2326bcdd23b8a08daf1b78f260c6
HassanMunene <awanzihassan@gmail.com>
2024-02-28 11:16:09 +0300

    adding onChange functionality to the SearchBar

--- KcaVibes/src/components/Searchbar.jsx
+++ KcaVibes/src/components/Searchbar.jsx
@@ -1,8 +1,18 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
 
 const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+  }
+
   return (
-    <form autoComplete="off" className="text-primary p-2 focus-within:text-black bg-accent1 sm:rounded-lg sm:ml-1 sm:mb-1">
+    <form onSubmit={handleSubmit} autoComplete="off" className="text-primary p-2 focus-within:text-black bg-accent1 sm:rounded-lg sm:ml-1 sm:mb-1">
       <label htmlFor="search-field"></label>
       <div className="flex flex-row justify-start items-center">
         <FiSearch color="black" aria-hidden="true" className="w-5 h-5 ml-4 font-bold"/>
@@ -11,7 +21,10 @@ const Searchbar = () => {
           autoComplete="off" 
           id="search-field" 
           placeholder="Search" 
-          className="flex-1 bg-transparent border-none outline-none text-black p-4"
+          className="flex-1 bg-transparent border-none outline-none text-black p-4 placeholder-black" 
+          type="search" 
+          value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
     </form>
