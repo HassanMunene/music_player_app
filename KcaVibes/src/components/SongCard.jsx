75d62a9571832e0319a3157dfac41819a5ce3986
HassanMunene <awanzihassan@gmail.com>
2024-03-01 15:05:10 +0300

    I have added more style to the discover section of the application

--- KcaVibes/src/components/SongCard.jsx
+++ KcaVibes/src/components/SongCard.jsx
@@ -16,32 +16,42 @@ const SongCard = ({song, isSongPlaying, activeSong, data}) => {
   };
 
   return (
-    <div className="w-[170px] p-4 bg-accent1 flex flex-col bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="w-full relative h-auto group">
-        <div 
-          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.track?.name === song.track.name ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-            <PlayPause 
-              song={song} 
-              handlePlay={handlePlayClick} 
-              handlePause={handlePauseClick} 
-              isSongPlaying={isSongPlaying} 
-              activeSong={activeSong}
-            />
+    <div className="w-[100%] p-4 bg-[#181818] hover:bg-[#3b3a3a] flex flex-col relative transition-background duration-300 ease-in animate-slideup rounded-lg cursor-pointer group">
+      <div className="w-full h-full flex flex-col">
+        <img alt="song_img" src={song.track.album.images[0].url} className="w-[92px] rounded-md shadow"/>
+        <div className="mt-4 flex flex-col">
+          <p className="font-semibold text-xl truncate text-white">
+            <Link to={`/songs/${song?.track?.id}`}>{song.track.name}</Link>
+          </p>
+          <p className="text-sm truncate text-white mt-1">
+            <Link to={song.track.artists ? `/artists/${song?.track?.artists[0]?.id}` : '/top-artists'}>
+              <span className="text-gray-500">song .</span>{" "}{song?.track?.artists[0].name}
+            </Link>
+          </p>
         </div>
-        <img alt="song_img" src={song.track.album.images[0].url}/>
-      </div>
-      <div className="mt-4 flex flex-col">
-        <p className="font-semibold text-lg truncate text-black">
-          <Link to={`/songs/${song?.track?.id}`}>{song.track.name}</Link>
-        </p>
-        <p className="text-sm truncate text-gray-700 mt-1">
-          <Link to={song.track.artists ? `/artists/${song?.track?.artists[0]?.id}` : '/top-artists'}>
-            {song?.track?.artists[0].name}
-          </Link>
-        </p>
+        <div className="hidden group-hover:block absolute right-[20px] bottom-[20px] shadow-lg translate-y-[8px] transition-all duration-300 ease-in">
+             <PlayPause 
+                song={song} 
+                handlePlay={handlePlayClick} 
+                handlePause={handlePauseClick} 
+                isSongPlaying={isSongPlaying} 
+                activeSong={activeSong}
+              />
+        </div>
       </div>
     </div>
   )
 };
 
+
+        // <div 
+        //   className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.track?.name === song.track.name ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+        //     <PlayPause 
+        //       song={song} 
+        //       handlePlay={handlePlayClick} 
+        //       handlePause={handlePauseClick} 
+        //       isSongPlaying={isSongPlaying} 
+        //       activeSong={activeSong}
+        //     />
+        // </div>
 export default SongCard;
--- KcaVibes/src/pages/Discover.jsx
+++ KcaVibes/src/pages/Discover.jsx
@@ -19,23 +19,11 @@ const Discover = () => {
 	//if (isFetching) return <Loader title="Loading songs..." />
 	//if (error) return <Error/>
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col min-h-0 max-w-full">
 			<div className="w-full mb-1 p-2 rounded-lg hidden sm:block">
 				<h2 className="font-bold text-3xl text-white text-left">Discover</h2>
 			</div>
-			<div className="flex flex-row items-center justify-start flex-wrap gap-3 sm:gap-5 sm:justify-start rounded-lg">
-{/*				{data?.map((song, index) => (
-					{index === 0 ? (
-						<SongCard 
-							key={song.track.id} 
-							song={song} 
-							isSongPlaying={isSongPlaying} 
-							activeSong={activeSong}
-							data={dummyData} 
-							index={index}
-						/>
-					): ("")}
-				))}*/}
+			<div className="h-full">
 				<SongCard 
 					key={data[0]?.track?.id} 
 					song={data[0]} 
@@ -49,3 +37,16 @@ const Discover = () => {
 }
 
 export default Discover;
+
+{/*				{data?.map((song, index) => (
+					{index === 0 ? (
+						<SongCard 
+							key={song.track.id} 
+							song={song} 
+							isSongPlaying={isSongPlaying} 
+							activeSong={activeSong}
+							data={dummyData} 
+							index={index}
+						/>
+					): ("")}
+				))}*/}
